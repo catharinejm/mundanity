dd320276064e93c3d5deb0482e49989b804921a1
Jon Distad <jon.distad@gmail.com>
2011-08-19 00:47:57 -0400

    Screen constants and conversion functions

--- src/mundanity.cljs
+++ src/mundanity.cljs
@@ -7,10 +7,44 @@
 
 (def game-window (gfx/createGraphics 800 600))
 
+(def scr-height 800)
+(def scr-width 600)
+(def scr-ratio (/ scr-width scr-height))
+(def fov-x (/ Math/PI 4))
+(def fov-y (/ fov-x scr-ratio))
+(def horizon 0.1)
+(def z-min (/ scr-width 2.0 (Math/tan (/ fov-x 2.0))))
+(def z-max (/ (* z-min (/ scr-height 2.0)) horizon))
+
+(defn screen-x [wx wz]
+  (+ (/ (* wx z-min) wz)
+      (/ scr-width 2.0)))
+
+(defn screen-y [wy wz]
+  (- (/ scr-height 2.0)
+     (* (- wy half-sht)
+        (/ z-min wz))))
+
+(defn world-x
+  ([sx sy] (world-x sx sy 0))
+  ([sx sy wy]
+     (let [half-sht (/ scr-height 2.0)
+           half-swd (/ scr-width 2.0)]
+       (/ (* (- sx half-swd)
+             (- wy half-sht))
+          (- half-sht sy)))))
+
+(defn world-z
+  ([sy] (world-z sy 0))
+  ([sy wy]
+     (let [half-sht (/ scr-height 2.0)]
+       (/ (* z-min (- wy half-sht))
+          (- half-sht sy)))))
+
 (.drawRect game-window
            0 300
            800 300
-           (gfx/Stroke. 1 "black")
+           (gfx/Stroke. 1 "blue")
            (gfx/SolidFill. "blue"))
 
 (.render game-window (.getElementById global/document "window"))
